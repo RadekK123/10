319820bc3056c29c629722af062628feb60a48dd
Radek Knapkiewicz <radknap@gmail.com>
2016-10-16 16:54:56 +0200

    version complite

--- 10.2/js/first.js
+++ 10.2/js/first.js
@@ -1,6 +1,10 @@
 //js file
 
+var promotion = 0.1;
+
+
 function Telefon (marka, cena, kolor) {
+	var self = this;
 	this.marka = marka;
 	this.cena = cena;
 	this.kolor = kolor;
@@ -11,9 +15,26 @@ function Telefon (marka, cena, kolor) {
 	}
 }
 
+var dane = {
+	cena : 20,
+	dana : 'coś'
+};
+
+function getWarranyCost() {
+	
+	var sale = (( this.cena) * promotion );
+	console.log( 'Kupując teraz telefon, gwarancję na kolejne 2 lata otrzymasz za: ' + sale + ' zł' );
+	return sale;
+}
+
+
+
 Telefon.prototype.printInfo = function() {
+
+	getWarranyCost.bind(Telefon(this.marka, this.cena, this.kolor));
 	var prise = this.isCheap ? 'Stosunkowo tani telefon' : 'Dość drogi telefon';
 	console.log('Marka telefonu to '+ this.marka + ' ma kolor '+ this.kolor + ' a jego cena wynosi ' + this.cena + ' złotych.' + prise + '.');
+	getWarranyCost();
 };
 
 var iPhone6s = new Telefon('Apple',2000,'czarny');
@@ -23,4 +44,6 @@ var samsungs6 = new Telefon('Samsung', 2100, 'niebieski');
 iPhone6s.printInfo();
 nokiaLumia.printInfo();
 samsungs6.printInfo();
+
+
 
