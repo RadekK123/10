a8a2f3b3668c61f6720b58fb6108015948810794
Radek Knapkiewicz <radknap@gmail.com>
2016-10-17 16:19:32 +0200

    fix to 10.3 files

--- 10.3/js/first.js
+++ 10.3/js/first.js
@@ -1,11 +1,18 @@
 //js file
 
 function Button (text) {
+	
 	this.text = text;
 }
 
 Button.prototype = {
 	create: function() {
+		
+		if(this.text == false) {
+			alert('Nie podałeś nazwy buttona');
+			this.text = 'Podaj naznę buttona';
+		}
+
 		var self = this;
 		this.$element = $('<button>');
 		this.$element.text(this.text);
@@ -13,12 +20,15 @@ Button.prototype = {
 		this.$element.click(function () {
 			alert(self.text);
 		});
-	$('body').append(this.$element);
+
+		$('body').append(this.$element);
 	}
 };
 
 var btn1 = new Button('Hello');
 var btn2 = new Button('Drugi :-p');
+var btn3 = new Button('');
 
 btn1.create();
 btn2.create();
+btn3.create();
